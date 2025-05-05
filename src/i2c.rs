dc87a91ebb3303234049b339133c8f8d3292fa4d
Albin Hedman <albin9604@gmail.com>
2025-05-05 22:33:52 +0200

    bump ci

--- src/i2c.rs
+++ src/i2c.rs
@@ -136,8 +136,7 @@ macro_rules! busy_wait {
                 $i2c.icr().write(|w| w.arlocf().clear());
                 return Err(Error::Arbitration);
             } else if isr.nackf().bit_is_set() {
-                $i2c.icr()
-                    .write(|w| w.stopcf().clear().nackcf().clear());
+                $i2c.icr().write(|w| w.stopcf().clear().nackcf().clear());
                 flush_txdr!($i2c);
                 return Err(Error::NotAcknowledge);
             } else {
